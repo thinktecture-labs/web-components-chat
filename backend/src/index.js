8ba9fbfdf471f9406869f86ad73c9668210eb4c6
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-09-03 15:26:08 +0200

    style(backend): reformat

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,32 +6,32 @@ const io = socketIo(port);
 const usernames = [];
 
 io.on('connection', socket => {
-    console.log('socket connected', socket.id);
+  console.log('socket connected', socket.id);
 
-    socket.userData = socket.userData || {};
+  socket.userData = socket.userData || {};
 
-    socket.on('online', username => {
-        console.log('socket sent online event with username', username);
+  socket.on('online', username => {
+    console.log('socket sent online event with username', username);
 
-        if (usernames.includes(username)) {
-            console.log('user', username, 'already exists, disconnecting...');
-            socket.emit('user-name-exists');
-            socket.disconnect(0);
-            return;
-        }
+    if (usernames.includes(username)) {
+      console.log('user', username, 'already exists, disconnecting...');
+      socket.emit('user-name-exists');
+      socket.disconnect(0);
+      return;
+    }
 
-        usernames.push(username);
-        socket.userData.username = username;
-        socket.join(username);
-        io.emit('user-join', {username});
-        socket.emit('all-users', { usernames })
-    });
+    usernames.push(username);
+    socket.userData.username = username;
+    socket.join(username);
+    io.emit('user-join', { username });
+    socket.emit('all-users', { usernames });
+  });
 
-    socket.on('disconnect', () => {
-        console.log('socket', socket.userData.username, 'disconnected');
-        usernames.splice(usernames.findIndex(username => username === socket.userData.username));
-        io.emit('user-leave', {username: socket.userData.username});
-    });
+  socket.on('disconnect', () => {
+    console.log('socket', socket.userData.username, 'disconnected');
+    usernames.splice(usernames.findIndex(username => username === socket.userData.username));
+    io.emit('user-leave', { username: socket.userData.username });
+  });
 });
 
 console.log('Server is up and running on port', port);
