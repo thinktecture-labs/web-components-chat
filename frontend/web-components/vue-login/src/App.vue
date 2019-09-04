7be6d66e9e251070753322c81b91a4df14396594
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-09-04 11:36:10 +0200

    feat(wc-vue): basic vue project

---
+++ frontend/web-components/vue-login/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import LoginForm from './components/LoginForm.vue';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+    LoginForm,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/web-components/vue-login/src/components/LoginForm.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'LoginForm',
+  };
+</script>
+
+<style scoped>
+
+</style>
