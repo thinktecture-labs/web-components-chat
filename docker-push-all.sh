8a7b323b552de6f3201d5510de4276b82a547de5
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-09-04 09:08:06 +0200

    build: fix docker push path

--- docker-push-all.sh
+++ docker-push-all.sh
@@ -3,4 +3,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin ttconferences.azurecr.io
 
 cd backend && bash docker-push.sh && cd ..
-cd frontend/ng-chat-app/docker-push.sh && cd ..
+cd frontend/apps/ng-chat-app && bash docker-push.sh && cd ..
