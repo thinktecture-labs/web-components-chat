844b75e6b2b62fc43452cc8f96bdc82384273796
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-09-05 11:59:20 +0200

    build: finish wc-angular build process

--- docker-build-all.sh
+++ docker-build-all.sh
@@ -10,6 +10,7 @@ cd frontend && npm i && cd ..
 cd frontend/web-components/react-contact-list && npm i && cd ../../..
 cd frontend/web-components/native-web-components && npm i && cd ../../..
 cd frontend/web-components/vue-login && npm i && cd ../../..
+cd frontend/web-components/angular-chat-window && npm i && cd ../../..
 cd frontend && npm run build-wc && cd ..
 
 # Host Apps Build
