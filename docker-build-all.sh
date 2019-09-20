28cf990e7413b9af31414832be18d557d7ac791f
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-09-20 11:56:32 +0200

    build: add lit element to docker build all command

--- docker-build-all.sh
+++ docker-build-all.sh
@@ -12,6 +12,7 @@ cd frontend/web-components/native-web-components && npm i && cd ../../..
 cd frontend/web-components/vue-login && npm i && cd ../../..
 cd frontend/web-components/angular-chat-window && npm i && cd ../../..
 cd frontend/web-components/stencil-components && npm i && cd ../../..
+cd frontend/web-components/lit-element-chat-link && npm i && cd ../../..
 cd frontend && npm run build-wc && cd ..
 
 # Host Apps Build
