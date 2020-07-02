5aef54dacc764e01329814b0f1f70a2284702607
Manuel Rauber <manuel.rauber@thinktecture.com>
2020-07-02 15:45:20 +0200

    fix(react): remove size from robohash url, it seems to fail quite often when using that query param

--- frontend/web-components/react-contact-list/src/avatar/Avatar.js
+++ frontend/web-components/react-contact-list/src/avatar/Avatar.js
@@ -11,7 +11,7 @@ export class Avatar extends React.Component {
 
     return (
       <div className={cssClasses}>
-        <img className={styles.avatar} src={`https://robohash.org/${this.props.name}.png?size=200x200&set=set4`}
+        <img className={styles.avatar} src={`https://robohash.org/${this.props.name}.png?set=set4`}
              alt={`Avatar of ${this.props.name}`}/>
       </div>
     );
