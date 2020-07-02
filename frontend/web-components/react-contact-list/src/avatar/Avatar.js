b0589bbd6045ed3e3619aa877f7de1b1dd51e9e6
Manuel Rauber <manuel.rauber@thinktecture.com>
2020-07-02 16:20:01 +0200

    fix(react): use identicon instead of robohash, it’s not reliable

--- frontend/web-components/react-contact-list/src/avatar/Avatar.js
+++ frontend/web-components/react-contact-list/src/avatar/Avatar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Avatar.module.css';
+import * as Identicon from 'identicon.js';
 
 export class Avatar extends React.Component {
   render() {
@@ -9,9 +10,15 @@ export class Avatar extends React.Component {
       cssClasses += ` ${styles['is-online']}`;
     }
 
+    // Just add a random GUID, because we need at least 15 characters for the hash.
+    const identicon = new Identicon(this.props.name + '12ff224f-4427-4621-9b2b-b61659d849d3', {
+      background: [0, 0, 0, 0],
+      size: 200
+    }).toString();
+
     return (
       <div className={cssClasses}>
-        <img className={styles.avatar} src={`https://robohash.org/${this.props.name}.png?set=set4`}
+        <img className={styles.avatar} src={`data:image/png;base64,${identicon}`}
              alt={`Avatar of ${this.props.name}`}/>
       </div>
     );
